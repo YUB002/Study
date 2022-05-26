a9801b7846812a0d9e2bec5e0ebde78cf288fa76
HYB <dob.zivot20@gmail.com>
2022-05-26 22:12:01 +0900

    자바 변수의 정의
    int 정수, double 실수, String 문자

---
+++ eclipse-workspace/helloworld/Variable.java
@@ -0,0 +1,16 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+				int a=1 ; // Numbeer -> integer ...-2, -1, 0, 1, 2...
+		        System.out.println(a); 
+		        
+	//	        int b =1.1; // 오류 real number-> double ...-2.0, -1.0, 0, 1.0, 2.0 ...
+		        double b= 1.1;
+		        System.out.println(b);
+		        
+		        String c="Hellow World";
+		        System.out.println(c);
+	}
+
+}
