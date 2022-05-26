a0678a2bec647186dfc5613d2b155669b9efc04a
HYB <dob.zivot20@gmail.com>
2022-05-26 22:17:13 +0900

    자바 변수의 효용
    자바 변수의 효용

---
+++ eclipse-workspace/helloworld/Letter.java
@@ -0,0 +1,12 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name ="Lisa";
+		System.out.println("Helloe, "+name+"..."+name+"..Lisa..Bye");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+	}
+
+}
