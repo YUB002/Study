28425c452945127bc94c67b4eeba705ce74975c2
HYB <dob.zivot20@gmail.com>
2022-08-03 22:23:40 +0900

    8.3
    제어문 while문 실습예제1

---
+++ Eclipse-workspace/academy/src/academy/Exam_8_3_01.java
@@ -0,0 +1,13 @@
+package academy;
+
+public class Exam_8_3_01 {
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		while (i<10) {
+			i++;
+			System.out.println("i = " + i);
+		}
+	}
+}
