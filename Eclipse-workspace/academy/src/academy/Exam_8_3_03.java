628f6a44abffae103225bbf69eb10396af17a068
HYB <dob.zivot20@gmail.com>
2022-08-03 22:53:13 +0900

    8.3
    제어문 while문

---
+++ Eclipse-workspace/academy/src/academy/Exam_8_3_03.java
@@ -0,0 +1,27 @@
+package academy;
+
+public class Exam_8_3_03 {
+	public static void main(String[] args) {
+		//1~10 출력		
+				int i = 0;
+				while (i<10) {
+					i++;
+					System.out.println(i);
+					}
+					
+				//10~1 출력
+				int n = 11;
+				while (n>1) {
+					n--;
+					System.out.println(n);
+					}
+					
+				//Hello World 6번 출력
+				int h = 0;
+				while(h<6) {
+					h++;
+					System.out.println("Hello World");
+					
+				}
+	}
+}
