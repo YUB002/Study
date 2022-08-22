4244bb9e9ef41aa1ca8bad70dc8c400c8f70fb9d
HYB <dob.zivot20@gmail.com>
2022-08-22 22:03:27 +0900

    8.22 arraylist

---
+++ Eclipse-workspace/academy/src/Day_12_StudentManager/src/Temp.java
@@ -0,0 +1,18 @@
+package Day_12_StudentManager.src;
+import java.util.ArrayList;
+
+public class Temp {
+	public static void main(String[] args) {
+		
+		ArrayList<String> arr = new ArrayList<>();
+		arr.add("Hello");
+		arr.add("World");
+		
+		
+		
+		//arr.remove(0);
+		arr.remove("Hello");
+		
+		
+	}
+}
